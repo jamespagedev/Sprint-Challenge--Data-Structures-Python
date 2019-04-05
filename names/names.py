15b6668b78c3b9136982c7d2ed835efbc1ee2f25
jamespagedev <jamespagedev@outlook.com>
2019-04-05 09:43:03 -0700

    names - MVP O(n)

--- names/names.py
+++ names/names.py
@@ -22,6 +22,7 @@ for name_1 in names_1:
 #-------------- First Optomized Solution O(n log n) -------------
 # "line in file" runtime is O(log n), which is nested within a for loop O(n), therefor...
 #   Runtime here would be O(n) * O(log n) -> O(n log n)
+"""
 with open('names_1.txt', 'r') as f1, open('names_2.txt', 'r') as f2:
         f2 = f2.read().splitlines()
         duplicates = []
@@ -29,6 +30,22 @@ with open('names_1.txt', 'r') as f1, open('names_2.txt', 'r') as f2:
         for line in f1.read().splitlines(): # O(n)
             if line in f2:                  # O(log n)
                 duplicates.append(line)
+"""
+
+#--------------------------- MVP O(n) ---------------------------
+# each file open is a single O(n) -> O(2n)
+# since both files lines are being read at the same time O(2n)...
+# We can conclude the final runtime formula is...
+# O(4n) -> O(n)
+with open('names_1.txt', 'r') as f1, open('names_2.txt', 'r') as f2:
+    dict_names = {}
+    for line1, line2 in zip(f1.read().splitlines(), f2.read().splitlines()):
+            dict_names[line1] = line2
+
+duplicates = []
+for value in dict_names.values(): # O(n)
+    if value in dict_names: # hash keys O(1)
+        duplicates.append(value) # O(1)
 
 
 end_time = time.time()
