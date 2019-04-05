6443d4273b37bbe4e80d47773484da2ed50e9272
jamespagedev <jamespagedev@outlook.com>
2019-04-05 08:21:07 -0700

    ring_buffer - All Tests (PASS)

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,25 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    # O(1)
+    def append(self, item):
+        # no overwriting data
+        if self.storage[self.current] is None:
+            self.storage[self.current] = item
 
-  def get(self):
-    pass
+        # reset current to first index
+        elif self.current == len(self.storage) - 1:
+            self.current = 0
+            self.storage[self.current] = item
+
+        # increment current by 1
+        else:
+            self.current += 1
+            self.storage[self.current] = item
+
+    # O(2n) = O(n)
+    def get(self):
+        return [x for x in self.storage[:self.current] if x] + [x for x in self.storage[self.current:] if x]
